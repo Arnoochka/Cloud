df4e5814f21f13cb3cb87af50647db2e8ef31a50
Victor <kolos.130278@gmail.com>
2023-11-03 15:27:25 +0300

    uodate

--- server/client.py
+++ server/client.py
@@ -2,7 +2,6 @@ import os
 import aiohttp
 import asyncio
 import aioconsole
-import queue
 
 class client:
     def __init__(self) -> None:
@@ -10,52 +9,48 @@ class client:
         self.password = "password"
         self.host = "127.0.0.1"
         self.port = 8888
-        url = 'http://localhost:8888'
-        self.tasks = queue.Queue()
+        self.url = 'http://localhost:8888'
         
         
-async def start_client(self):
-    
-    while True:
+    async def start_client(self):
         if await self.autorization():
-            command = aioconsole.ainput()
-            if command == "/send": 
-                await self.send_command(command)
-                await self.send_file()
-            else:
-                print("неизвестная команда")
-        else:
-            return
+            while True:
+                    command = await aioconsole.ainput()
+                    if command == "/send": 
+                        await self.send_file()
+                    else:
+                        print("неизвестная команда")
              
-async def autorization(self)->bool:
-    async with aiohttp.ClientSession() as session:
-        async with session.post(self.url, data = {'login': self.login, 'password':self.password}) as response:
-            return int(await response.text()) == 200
+    async def autorization(self)->bool:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{self.url}/login" , data = {'login': self.login, 'password':self.password}) as response:
+                code = await response.text()
                 
-async def send_command(self, command):
-    async with aiohttp.ClientSession() as session:
-        data = {"command": command}
-        async with session.post(self.url, json = data) as response:
-            code = int(await response.text())
-            if code == 200:
-                print("команда успешно отправлена")
-            else:
-                print("ошибка")
-    
-async def send_file(self):
-    await self.send_command("/send file")
-    with open('file directory', 'rb') as file:
-        data = {'file':file.read(), 'name': 'file_name', 'login': self.login}
-        
-    async with aiohttp.ClientSession() as session:
-        async with session.post(self.url, data = data) as response:
-            code = int(await response.text())
-            if code == 200:
-                print("файл успешно отправлен")
-            else:
-                print("ошибка")
+                return int(code) == 200
+
+    async def send_command(self, command):
+        async with aiohttp.ClientSession() as session:
+            data = {"command": command}
+            async with session.post(f"{self.url}/send_file", json = data) as response:
+                code = await response.text()
+                if int(code) == 200:
+                    print("команда успешно отправлена")
+                else:
+                    print("ошибка")
+
+    async def send_file(self):
+        with open('Cloud/server/image.jpg', 'rb') as file:
+            data = {'file':file.read(), 'name': 'file_name.jpg'}
+            print(data)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{self.url}/send_file", data = data) as response:
+                code = int(await response.text())
+                if code == 200:
+                    print("файл успешно отправлен")
+                else:
+                    print("ошибка")
 
 
 if __name__=="__main__":    
-    
-    asyncio.run(client().start_client())
+    my_client = client()
+    asyncio.run(my_client.start_client())
--- server/server.py
+++ server/server.py
@@ -1,50 +1,146 @@
+import os
 import asyncio
 from aiohttp import web
-import aiomysql
-import multiprocessing
+from aiohttp import ClientSession
+from aiohttp import FormData
+from aiohttp import WSMsgType
+import aioconsole
+import asyncpg
+from multiprocessing import Process
 
-
-async def handle_post_request(request):
-    data = await request.post()
     
-    # Получаем картинку из данных запроса
-    image = data['file']
-    image_data = image.file.read()
-    
-    # Сохраняем картинку на сервере
-    with open(f"{data['name']}", 'wb') as f:
-        f.write(image_data)
-    
-    # Возвращаем ответ клиенту
-    return web.Response(200)
-
 class input_server:
-    def __init__(self, host, port) -> None:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
         self.app = web.Application()
-        self.app.router.add_post('/', handle_post_request)
-        self.runner = web.AppRunner(self.app)
-        self.site = web.TCPSite(self.runner, host, port)
-        self.users = dict()
-        self.users_on_server = dict()
-        self.users_cloud = dict()
+
+        self.app.router.add_post('/login', self.autorization)
+        self.app.router.add_post('/send_file', self.send_file)
+        self.app.router.add_get("/name_files", self.send_file)
+        self.app.middlewares.append(self.middleware)
         
+        self.bearer_token = "he45stogddf8g70sd7g0g7sd07gs05"
+        
+        self.url_controller = ""
+        
+    async def middleware(self, app, heandler: callable, request: web.Request)->web.Response:
+        if request.path == "/login":
+            response = await heandler(request)
+        elif request.headers.get("Autorization"):
+            if self.bearer_token == request["Autorization"]:
+                response = await heandler(request)
+            else:
+                return web.Response(status=401, body="you not autorization")
+        else:
+            return web.Response(status=401, body="you not autorization")
+        
+        return response
+            
     async def start_server(self):
-        
+        self.runner = web.AppRunner(self.app)
         await self.runner.setup()
+        self.site = web.TCPSite(self.runner, self.host, self.port)
         await self.site.start()
         
-        
-    async def connect(self, request):
-        data = await request.post()
-        pass
-    async def using():
-        pass
+        await aioconsole.aprint(f"сервер запущен c портом {self.port}")
 
-class output_server(self, host, port):
-    def __init__(self, host, port):
-        pass
+    async def autorization(self, request: web.Request)->web.Response:
+        
+        if not request.headers.get('login') or not request.headers.get('password'):
+            return web.Response(status=400, body="no login or password entered")
+        
+        data = await request.post()
+        
+        
+        
+        print(data['login'], data['password'])
+        
+        return web.Response(status=200, body="autorization successfully")
     
+    async def send_file(self, request: web.Request)->web.Response:
+        
+        not_atribute = await self.checking_for_attributes(['login', 'file', 'filename'], request)
+        if not_atribute[0]:
+            return web.Response(status=401, body=f"not received {not_atribute[1]}")
+            
+        data = await request.post()
+
+        # отправка данных контроллеру
+        
+        async with ClientSession() as session:
+            async with session.post(f"{self.url_controller}/send_file", data = data) as response:
+                return await response
+            
+    async def get_file_name(self, request: web.Request):
+        
+        not_atribute = await self.checking_for_attributes(['login', 'files'], request)
+        if not_atribute[0]:
+            return web.Response(status=401, body=f"not received {not_atribute[1]}")
+        
+        data = await request.get()
+        
+        #получение имен файлов от сервера
+        async with ClientSession() as session:
+            async with session.get(f"{self.url_controller}/get_file_name", params = data) as response:
+                return await response
+            
+    # async def get_files(self, request: web.Request):
+        
+    #     not_atribute = await self.checking_for_attributes(['login', 'files'], request)
+    #     if not_atribute[0]:
+    #         return web.Response(status=401, body=f"not received {not_atribute[1]}")
+    #     write_file = Process(target = self.get_file, args=(request, )).start()
+        
+    #     names_get_files = list()
+        
+    #     read_and_det_files = Process(target=self.get_file, args=(request, names_get_files))
+    #     while not read_and_det_files.is_alive():
+    #         await asyncio.sleep(0.1)
+    #     return web.Response(status=200, body=names_get_files)
+        
+        
+    # async def get_file(self, request: web.Request, names_get_files):
+        
+    #     ws = web.WebSocketResponse()
+    #     await ws.prepare(request)
+        
+    #     async for msg in ws:
+    #         if msg.type == WSMsgType.TEXT:
+    #             filenames = msg.data.split(",")  # получение списка имен файлов из сообщения
+                
+    #         for filename in filenames:
+    #             with open(filename, 'rb') as file:
+    #                 await ws.send_bytes(file.read())  # отправка содержимого каждого файла обратно клиенту
+    async def get_files(self, request: web.Request):
+        
+        not_atribute = await self.checking_for_attributes(['login', 'files'], request)
+        if not_atribute[0]:
+            return web.Response(status=401, body=f"not received {not_atribute[1]}")
+        async with ClientSession() as session:
+            async with session.post(f"{self.url_controller}/get_file_name", data = request['data']) as response:
+                return await response
+        
+            
+    async def checking_for_attributes(self, atrubutes: list, request: web.Request)->tuple:
+        for atribute in list:
+            if not request.headers.get(atribute):
+                return (1, atribute)
+        return (0, "not")
+
+            
+
+async def main():
+    my_input_servers = input_server('localhost', 8888)
+    tasks = [
+        asyncio.create_task(my_input_servers.start_server())
+    ]
+    await asyncio.gather(*tasks)
     
-loop = asyncio.get_event_loop()
-loop.run_until_complete(input_server('localhost', 8888).connect())
-loop.run_forever()
+    while True:
+        command = await aioconsole.ainput()
+        if command == "/stop":
+            break
+    
+if __name__ == "__main__":
+    asyncio.run(main())
