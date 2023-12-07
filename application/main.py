7bfe6510448faf67940b27af3332c81197e4c3b9
Stephan88 <127399395+StephanOlizko@users.noreply.github.com>
2023-12-07 22:59:50 +0300

    Update on application

--- application/main.py
+++ application/main.py
@@ -7,6 +7,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.label import Label
+from kivy.uix.recycleview import RecycleView
+from kivy.properties import StringProperty
 from kivy.lang import Builder
 import requests
 import aiohttp
@@ -27,7 +29,10 @@ class MainScreen(BoxLayout):
 login = ""
 password = ""
 token = ""
-ip = "192.168.154.5"
+ip_list = ["192.168.154.5"]
+ip = ""
+files = {"link": "1.txt",
+         "link1": "2.txt"}
 
 
 class AuthorizationScreen(BoxLayout):
@@ -35,16 +40,74 @@ class AuthorizationScreen(BoxLayout):
         global login
         global password
         global token
+        global ip_list
         global ip
-
         login = self.ids.login_input.text
         password = self.ids.password_input.text
 
         data = {'login': login, 'password': password}
-        response = requests.post(f'http://{ip}:8080/login', json=data)
-        token = response.content.decode()
+        for ipp in ip_list:
+            try:
+                response = requests.post(f'http://{ipp}:8080/login', json=data)
+            except:
+                pass
+            else:
+                if response.status_code == 200:
+                    token = response.content.decode()  
+                    ip = ipp            
+                    break
 
-        print(response.content.decode())
+
+class FileViewScreen(BoxLayout):
+    def on_enter(self):
+        super().on_enter()
+        self.update_file_list()
+
+    def update_file_list(self):
+        global files
+        
+        recycle_view = self.ids.file_view_rv
+
+        # Очищаем текущие виджеты
+        recycle_view.data = []
+
+
+        for link in files:
+            file_name = files[link]
+            recycle_view.data.append({'file_name': file_name, 'delete_button': 'Delete', 'download_button': 'Download', 'link': link})
+
+
+    def delete_file(self, instance):
+        print(instance)
+
+
+    def download_file(self, instance):
+        global files
+
+        path = files[instance]
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.post(link) as response:
+                with open(path, 'wb') as file:
+                    while True:
+                        chunk = await response.content.read(1024)
+                        if not chunk:
+                            break
+                        file.write(chunk)
+        """
+        response = requests.post(instance)
+        with open(path, 'wb') as file:
+            while True:
+                chunk =  response.content.read(1024)
+                if not chunk:
+                    break
+                file.write(chunk)
+
+
+class FileViewRow(BoxLayout):
+    file_name = StringProperty()
+    delete_button = StringProperty()
+    download_button = StringProperty()
 
 class FileUploadScreen(BoxLayout):
     def open_file_chooser(self):
@@ -68,6 +131,7 @@ class FileUploadScreen(BoxLayout):
         global token
 
         file_path:str = value[0]
+        file_name = file_path
 
         with io.open(file_path, "rb") as file:
             new_file = ["start".encode()]
@@ -89,9 +153,10 @@ class FileUploadScreen(BoxLayout):
             print(response.content.decode())
             url = response.content.decode()
 
-            asyncio.run(self.upload_chunks(new_file, data, url))
+            asyncio.run(self.upload_chunks(new_file, data, url, file_name))
 
-    async def upload_chunks(self, new_file, data, url):
+    async def upload_chunks(self, new_file, data, url, file_name):
+        global files
         """
         async with aiohttp.ClientSession() as session:
             for chunk in new_file:
@@ -106,7 +171,10 @@ class FileUploadScreen(BoxLayout):
         """
         for chunk in new_file:
             response = requests.post(f"http://{url}/send_file", data=data, files={"file_chunk": chunk})
+            files[response.content.decode()] = file_name
             print(response.content)
+
+
 
 if __name__ == '__main__':
     KeyValueApp().run()
